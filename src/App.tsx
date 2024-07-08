5ef9f68dc272957ce54362c13c36e15e817eaab6
S. David Kyle <sdavidkyle@gmail.com>
2024-07-08 13:04:29 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="p-10">
+      <Button>What's up suckas!</Button>
+    </div>
+  );
+}
+
+export default App;
