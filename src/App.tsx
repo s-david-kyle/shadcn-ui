dd628e6967d04592dd6cc1c14f3843e9261609af
S. David Kyle <sdavidkyle@gmail.com>
2024-07-09 09:25:33 -0700

    adding config

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import { Button } from "@/components/ui/button";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Terminal } from "lucide-react";
 
 function App() {
   return (
-    <>
-      <div className="p-10">
-        <Button>What's up suckas!</Button>
-      </div>
-      <Alert>
-        <Terminal className="h-4 w-4" />
-        <AlertTitle>Heads up!</AlertTitle>
-        <AlertDescription>
-          You can add components and dependencies to your app using the cli.
-        </AlertDescription>
-      </Alert>
-    </>
+    <div className="p-10">
+      <Button variant="outline">Hi dere!</Button>
+    </div>
   );
 }
 
